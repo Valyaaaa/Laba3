f201cdf7206442ebe2d8349832e8eadabd5ef533
Yuliia Petrova <yuliiatrova@gmail.com>
2024-02-10 20:34:03 +0200

    Added new function(input number is prime or not) to utils.py

--- utils.py
+++ utils.py
@@ -1,5 +1,15 @@
-def factorial(n):
-    a = 1
-    for i in range(1,n+1):
-        a = a * i
-    return a
+#перевірка чи є число простим
+
+n = int(input())
+
+is_prime = True
+
+for i in range (2, (n//2) + 1):
+    if n % i == 0:
+        is_prime = False
+        break
+
+if is_prime:
+    print("Введене число є простим")
+else:
+    print("Введене число НЕ є простим")
