2c9f72fc70eb310db4b3eaaaf51cf75fbdd34d2a
Egor Orlik <egororlik27@knu,ua>
2024-02-13 19:10:35 +0200

    add new func and command

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
 import utils
 n = int(input())
 print(utils.factorial(n))
+
+#виклик функції степеня п'ятірки
+num = int(input())
+print(utils.is_power_of_five(num))
--- utils.py
+++ utils.py
@@ -3,3 +3,20 @@ def factorial(n):
     for i in range(1,n+1):
         a = a * i
     return a
+
+def is_power_of_five(num):
+    degree = 0
+    while 5 ** degree <= num:
+        if 5 ** degree == num:
+            return True
+        degree += 1
+
+    return False
+
+number = int(input("Введіть число: "))
+result = is_power_of_five(number)
+
+if result:
+    print(f"{number} є степенем п'ятірки.")
+else:
+    print(f"{number} не є степенем п'ятірки.")
